8fe1821eae43b335e4675262190a315a108e5ab4
Casper Larsson <73782366+casperlarsson-bit@users.noreply.github.com>
2023-09-22 13:08:02 +0200

    Added piece preview

--- js/gameRenderer.js
+++ js/gameRenderer.js
@@ -22,23 +22,38 @@ class GameRenderer {
         for (let row = 0; row < this.grid.rows; ++row) {
             for (let column = 0; column < this.grid.columns; ++column) {
                 // There is a block at current grid, draw it!
-                if (this.grid.cells[row][column]) {
-                    this.gridContext.fillStyle = this.grid.cells[row][column]
-                    this.gridContext.fillRect(this.grid.blockSize * column, this.grid.blockSize * row, this.grid.blockSize, this.grid.blockSize)
-                    this.gridContext.strokeStyle = BORDER_COLOR
-                    this.gridContext.strokeRect(this.grid.blockSize * column, this.grid.blockSize * row, this.grid.blockSize, this.grid.blockSize)
-                }
+                if (!this.grid.cells[row][column]) continue
+
+                this.gridContext.fillStyle = this.grid.cells[row][column]
+                this.gridContext.fillRect(this.grid.blockSize * column, this.grid.blockSize * row, this.grid.blockSize, this.grid.blockSize)
+                this.gridContext.strokeStyle = BORDER_COLOR
+                this.gridContext.strokeRect(this.grid.blockSize * column, this.grid.blockSize * row, this.grid.blockSize, this.grid.blockSize)
+
             }
         }
     }
 
     redrawNextPiece(piece) {
+        if (!this.nextPieceContext) throw new ReferenceError('No piece context exists')
+
+        const numRows = piece.shape.length
+        const numColumns = piece.shape[0].length
+
         this.nextPieceContext.clearRect(0, 0, this.nextPieceCanvas.width, this.nextPieceCanvas.height)
 
-        this.nextPieceContext.fillStyle = piece.color
-        this.nextPieceContext.fillRect(this.grid.blockSize * 1, this.grid.blockSize * 1, this.grid.blockSize, this.grid.blockSize)
-        this.nextPieceContext.strokeStyle = BORDER_COLOR
-        this.nextPieceContext.strokeRect(this.grid.blockSize * 1, this.grid.blockSize * 1, this.grid.blockSize, this.grid.blockSize)
+        const offsetX = (this.nextPieceCanvas.width - this.grid.blockSize * numColumns) / 2
+        const offsetY = (this.nextPieceCanvas.height - this.grid.blockSize * numRows) / 2
+
+        for (let row = 0; row < numRows; ++row) {
+            for (let column = 0; column < numColumns; ++column) {
+                if (!piece.shape[row][column]) continue
+
+                this.nextPieceContext.fillStyle = piece.color
+                this.nextPieceContext.fillRect(this.grid.blockSize * column + offsetX, this.grid.blockSize * row + offsetY, this.grid.blockSize, this.grid.blockSize)
+                this.nextPieceContext.strokeStyle = BORDER_COLOR
+                this.nextPieceContext.strokeRect(this.grid.blockSize * column + offsetX, this.grid.blockSize * row + offsetY, this.grid.blockSize, this.grid.blockSize)
+            }
+        }
     }
 
     // Update the displayed score on the page
